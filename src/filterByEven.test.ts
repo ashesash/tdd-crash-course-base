455dbe41dc95138cba6419570703a0a520788702
ashesash <aishwarya.s@live.com>
2023-03-04 11:01:30 +1100

    wrote tests

---
+++ src/filterByEven.test.ts
@@ -0,0 +1,9 @@
+import {filterByEven} from "./filterByEven";
+
+test(' given [1,2,3,4,5,6], when filter by even, then [2,4,6]', () => {
+    expect(filterByEven([1,2,3,4,5,6])).toEqual([2,4,6]);
+});
+
+test(' given [], when filter by even, then []', () => {
+    expect(filterByEven([])).toThrowError('empty array is not acceptable');
+});
---
+++ src/filterByEven.ts
@@ -0,0 +1,4 @@
+export function filterByEven(numbers: number[]): number[] {
+    return numbers.filter(num => num % 2 === 0);
+}
+
